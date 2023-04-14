3f0d74b6b768274693999544ba4b63fbbd4a9c60
willian1997 <ihodmo@gmail.com>
2023-04-14 19:29:11 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'reqres',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
